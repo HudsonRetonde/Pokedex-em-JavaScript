80803eee994286379de8d05ccf33aa0a45a6b4e1
Hudson Retonde <hudsonsretonde@gmail.com>
2022-11-21 22:18:12 -0300

    fetch.

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,7 @@
-console.log("Estou aqui!")
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url).then(function(response){
+    console.log(reponse);
+});
