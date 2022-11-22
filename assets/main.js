b289922e47e4de41ccb5db6f939a0638108530b1
Hudson Retonde <hudsonsretonde@gmail.com>
2022-11-22 15:20:39 -0300

    Separando o consumo da API da manipulação do HTML.

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,4 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
 
 function convertPokemonToHtml(pokemon) {
    return `
@@ -20,14 +18,16 @@ function convertPokemonToHtml(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList');
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {        
+pokeApi.getPokemons().then((pokemons) => {     
+    
+        const listaItens = []
+
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons [i];
-            pokemonList.innerHTML += convertPokemonToHtml(pokemon);
+            listaItens.push(convertPokemonToHtml(pokemon))
         }
+
+        console.log(listaItens)
     })
     .catch((error) => console.log(error))
     
